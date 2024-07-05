ebf3982f0f8eff2858bc6d5aa906da7b22d6d2d7
BelenAraceliLagos <belen.lagos.c@gmail.com>
2024-07-04 21:58:18 -0400

    nuevo comit

---
+++ eclipse-workspace/HelloWorldServlet/src/main/java/web/HelloWorld.java
@@ -0,0 +1,22 @@
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet{
+	
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response ) throws IOException{
+		response.setContentType("text/html/charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("Hello World from Servlet");
+	}
+
+}
---
+++ eclipse-workspace/HelloWorldServlet/src/main/webapp/index.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet"
+	href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
+<title>Hello World Servlets</title>
+</head>
+<body>
+	<main class="container">
+		<h1>
+			Pulsa el botón para llamar al <strong>Servlet</strong>
+		</h1>
+		<a href="/HelloWorldServlet/HelloWorld" role="button">Servlet
+			HelloWorld</a>
+	</main>
+</body>
+</html>
